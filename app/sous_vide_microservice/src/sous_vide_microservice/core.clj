da899fe6e3c08b0e359aa8c2f4bab4edc11e8e74
emulebest <igorkorotach@gmail.com>
2019-05-10 13:08:35 -0700

    added redis support

--- app/sous_vide_microservice/src/sous_vide_microservice/core.clj
+++ app/sous_vide_microservice/src/sous_vide_microservice/core.clj
@@ -1,22 +1,27 @@
 (ns sous-vide-microservice.core
   (:require [taoensso.carmine :as car :refer (wcar)]
             [clojure.core.async :as a :refer [>! <! >!! <!! go chan buffer close! thread
-                                              alts! alts!! timeout]]))
+                                              alts! alts!! timeout go-loop]]))
 
 (defn get-msg
   [topic conn ch]
   (car/with-new-pubsub-listener
     (:spec conn)
-    {topic #(go (>! ch %))}
+    {topic (fn [msg] (do
+                       (println "Got " msg)
+                       (go (>! ch msg))))}
     (car/subscribe topic)))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
-  (let [server1-conn {:host "localhost" :port 6379}
+  (let [server1-conn {:pool {} :spec {:uri "redis://redis:6379"}}
         c1 (chan)
         listener (get-msg "sous-vide" server1-conn c1)]
-    (go (println (<!! c1)))
+    (go-loop []
+      (let [[_ _ msg] (<! c1)]
+        (println msg)
+        (recur)))
     ))
 
 
